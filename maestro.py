8f72b2dfd410cce72749ace31ccb4df6825dc9af
Dr. Zej. B <drzejb@borowiec.ca>
2019-08-03 19:17:53 -0400

    code cleanup

--- maestro.py
+++ maestro.py
@@ -1,17 +1,11 @@
 import serial
 import time
+import json
 from sys import version_info
 import os
+import logging
 
 PY2 = version_info[0] == 2  # Running Python 2.x?
-
-trajectory = [
-    [1500, 1500, 1500, 1500, 1500],
-    [2500, 1500, 1500, 1500, 1500],
-    [1000, 1500, 1500, 1500, 1500],
-    [1500, 1500, 1500, 1500, 1500],
-]
-
 
 #
 # ---------------------------
@@ -28,21 +22,50 @@ trajectory = [
 #
 
 str_to_byte = {
-
 }
 
+DEFAULT_CONFIG = {
+    'min': [500, 500, 500, 500, 500, 500],
+    'max': [2500, 2500, 2500, 2500, 2500, 2500],
+    'home': [1500, 1500, 1500, 1500, 1500, 1500],
+    'speed': [0, 0, 0, 0, 0, 0],
+    'accel': [0, 0, 0, 0, 0, 0],
+    'speed_adjust' : 1,
+    'num_of_channels' : 6
+}
+
+def load_config_file():
+    """
+    Loads config file in json format and returns config dictionary
+    """
+    if os.path.isfile('config.json'):
+        try:
+            fid = open('config.json','r')
+            out = fid.read()
+            fid.close()
+            config = json.loads(out)
+            return config
+        except:            
+            logging.error("cannot decode maestro config.json file - using DEFAULT_CONFIG")
+            
+    else:
+        logging.error("cannot find maestro config.json file in the directory - using DEFAULT_CONFIG")
+    return DEFAULT_CONFIG
+
 class Controller:
-    # When connected via USB, the Maestro creates two virtual serial ports
-    # /dev/ttyACM0 for commands and /dev/ttyACM1 for communications.
-    # Be sure the Maestro is configured for "USB Dual Port" serial mode.
-    # "USB Chained Mode" may work as well, but hasn't been tested.
-    #
-    # Pololu protocol allows for multiple Maestros to be connected to a single
-    # serial port. Each connected device is then indexed by number.
-    # This device number defaults to 0x0C (or 12 in decimal), which this module
-    # assumes.  If two or more controllers are connected to different serial
-    # ports, or you are using a Windows OS, you can provide the tty port.  For
-    # example, '/dev/ttyACM2' or for Windows, something like 'COM3'.
+    """
+    When connected via USB, the Maestro creates two virtual serial ports
+    /dev/ttyACM0 for commands and /dev/ttyACM1 for communications.
+    Be sure the Maestro is configured for "USB Dual Port" serial mode.
+    "USB Chained Mode" may work as well, but hasn't been tested.
+    
+    Pololu protocol allows for multiple Maestros to be connected to a single
+    serial port. Each connected device is then indexed by number.
+    This device number defaults to 0x0C (or 12 in decimal), which this module
+    assumes.  If two or more controllers are connected to different serial
+    ports, or you are using a Windows OS, you can provide the tty port.  For
+    example, '/dev/ttyACM2' or for Windows, something like 'COM3'.
+    """
     def __init__(self, tty_str='/dev/ttyACM0', device=0x0c):
 
         self.tty_str = tty_str
@@ -62,24 +85,37 @@ class Controller:
         # Servo minimum and maximum targets can be restricted to protect components.
         self.Mins = [0] * 24
         self.Maxs = [0] * 24
+        self.config = load_config_file()
 
         self.establish_connection()
+        
+        if not self.tty_port_connection_established:
+            logging.error("Did not establish connection during object initialization")
 
-    def establish_connection(self):
+    def establish_connection(self):        
         try:
-            self.usb = serial.Serial(self.tty_str, timeout=1)
-            self.tty_port_exists = True
-            self.tty_port_connection_established = True
+            if os.path.exists(self.tty_str):
+                self.usb = serial.Serial(self.tty_str, timeout=1)
+                self.tty_port_exists = True
+                self.tty_port_connection_established = True
+            else:
+                logging.error('Specified serial port does not exist')
         except Exception as e:
             self.last_exception = e
-
-    # Cleanup by closing USB serial port
+            logging.error("Cannot connect to the controller. last_exception = {}".format(e))
+    
+    def reload_default_config(self):
+        self.config = load_config_file()
+    
     def close(self):
-        self.usb.close()
-
-    # Send a Pololu command out the serial port
+        """ Cleanup by closing USB serial port"""
+        if self.tty_port_connection_established:
+            self.usb.close()
+    
     def send(self, cmd):
+        """ Send a Pololu command out the serial port """ 
         self.last_cmd_send = cmd
+        logging.debug("send({})".format(cmd))
         if self.establish_connection:
             cmd_str = self.pololu_cmd + cmd
             if PY2:
@@ -87,68 +123,80 @@ class Controller:
             else:
                 out = self.usb.write(bytes(cmd_str, 'latin-1'))
         else:
+            logging.warning("Cannot send command connection is not established")
             out = -1
         return out
 
     def read(self):
-
-        if self.usb.is_open:
-
-            start_time = time.time()
-            while time.time() - start_time < self.timeout:
-                if self.usb.in_waiting == 1:
-                    return ord(self.usb.read())
+        """ Read a Pololu response """ 
+        response = -1
+        if self.establish_connection:
+            if self.usb.is_open:
+                start_time = time.time()
+                while time.time() - start_time < self.timeout:
+                    if self.usb.in_waiting == 1:
+                        response = ord(self.usb.read())
+                        logging.debug("read() = {}".format(response))
+            else:
+                logging.warning("Cannot use read command when the port is closed")
         else:
-            return -1
-
-    # Set channels min and max value range.  Use this as a safety to protect
-    # from accidentally moving outside known safe parameters. A setting of 0
-    # allows unrestricted movement.
-    #
-    # ***Note that the Maestro itself is configured to limit the range of servo travel
-    # which has precedence over these values.  Use the Maestro Control Center to configure
-    # ranges that are saved to the controller.  Use setRange for software controllable ranges.
-    def setRange(self, chan, min, max):
-        self.Mins[chan] = min
-        self.Maxs[chan] = max
-
-    # Return Minimum channel range value
-    def getMin(self, chan):
+            logging.warning("Cannot use read command when connection is not established")
+        return response
+   
+    def set_range(self, chan, min, max):
+        """ Set channels min and max value range.  Use this as a safety to protect
+        from accidentally moving outside known safe parameters. A setting of 0
+        allows unrestricted movement.
+        
+        ***Note that the Maestro itself is configured to limit the range of servo travel
+        which has precedence over these values.  Use the Maestro Control Center to configure
+        ranges that are saved to the controller.  Use set_range for software controllable ranges.
+        """
+        if chan >=0 and chan <=24:
+            self.Mins[chan] = min
+            self.Maxs[chan] = max
+        else:
+            logging.error("Specified channel is out of range")
+    
+    def get_min(self, chan):
+        """ Return Minimum channel range value"""
         return self.Mins[chan]
 
-    # Return Maximum channel range value
     def getMax(self, chan):
+        """ Return Maximum channel range value """
         return self.Maxs[chan]
 
-    # Set channel to a specified target value.  Servo will begin moving based
-    # on Speed and Acceleration parameters previously set.
-    # Target values will be constrained within Min and Max range, if set.
-    # For servos, target represents the pulse width in of quarter-microseconds
-    # Servo center is at 1500 microseconds, or 6000 quarter-microseconds
-    # Typcially valid servo range is 3000 to 9000 quarter-microseconds
-    # If channel is configured for digital output, values < 6000 = Low ouput
     def set_target(self, chan: object, target: object) -> object:
+        """
+        Set channel to a specified target value.  Servo will begin moving based
+        on Speed and Acceleration parameters previously set.
+        Target values will be constrained within Min and Max range, if set.
+        For servos, target represents the pulse width in of quarter-microseconds
+        Servo center is at 1500 microseconds, or 6000 quarter-microseconds
+        Typcially valid servo range is 3000 to 9000 quarter-microseconds
+        If channel is configured for digital output, values < 6000 = Low ouput
+        """
         self.target_positions[chan] = target
-        target = round(target * 4);
+        target = round(target * 4)
         # if Min is defined and Target is below, force to Min
         if self.Mins[chan] > 0 and target < self.Mins[chan]:
             target = self.Mins[chan]
+        
         # if Max is defined and Target is above, force to Max
         if self.Maxs[chan] > 0 and target > self.Maxs[chan]:
             target = self.Maxs[chan]
-        #
+        
         lsb = target & 0x7f  # 7 bits for least significant byte
         msb = (target >> 7) & 0x7f  # shift 7 and take next 7 bits for msb
         cmd = chr(0x04) + chr(chan) + chr(lsb) + chr(msb)
         self.send(cmd)
         # Record Target value
 
-
     def set_target_vector(self, targets, sleep_time=0):
         for chan, pos in enumerate(targets):
             self.set_target(chan, pos)
         
-        pause_sec = calculate_movement_time(get_max_angle(self.last_set_target_vector, targets))
+        pause_sec = get_slowest_movement_time(self.last_set_target_vector, targets, self.config['speed'])
         print(pause_sec)
         time.sleep(pause_sec)
 
@@ -157,68 +205,73 @@ class Controller:
         # while self.getMovingState(): #and time.time() - to < timeout:
         #     pass
 
-    def run_trajectory(self, trajectory=trajectory):
+    def run_trajectory(self, trajectory):
         for new_target_vector in trajectory:
             self.set_target_vector(new_target_vector)
-            
-    # Set speed of channel
-    # Speed is measured as 0.25microseconds/10milliseconds
-    # For the standard 1ms pulse width change to move a servo between extremes, a speed
-    # of 1 will take 1 minute, and a speed of 60 would take 1 second.
-    # Speed of 0 is unrestricted.
-    def setSpeed(self, chan, speed):
+   
+    def set_speed(self, chan, speed):
+        """
+        Set speed of channel
+        Speed is measured as 0.25microseconds/10milliseconds
+        For the standard 1ms pulse width change to move a servo between extremes, a speed
+        of 1 will take 1 minute, and a speed of 60 would take 1 second.
+        Speed of 0 is unrestricted.
+        """
         lsb = speed & 0x7f  # 7 bits for least significant byte
         msb = (speed >> 7) & 0x7f  # shift 7 and take next 7 bits for msb
         cmd = chr(0x07) + chr(chan) + chr(lsb) + chr(msb)
         self.send(cmd)
+        self.config['speed'][chan] = speed
 
-    # Set acceleration of channel
-    # This provide soft starts and finishes when servo moves to target position.
-    # Valid values are from 0 to 255. 0=unrestricted, 1 is slowest start.
-    # A value of 1 will take the servo about 3s to move between 1ms to 2ms range.
     def setAccel(self, chan, accel):
+        """
+        Set acceleration of channel
+        This provide soft starts and finishes when servo moves to target position.
+        Valid values are from 0 to 255. 0=unrestricted, 1 is slowest start.
+        A value of 1 will take the servo about 3s to move between 1ms to 2ms range.
+        """
         lsb = accel & 0x7f  # 7 bits for least significant byte
         msb = (accel >> 7) & 0x7f  # shift 7 and take next 7 bits for msb
         cmd = chr(0x09) + chr(chan) + chr(lsb) + chr(msb)
         self.send(cmd)
 
-    # Get the current position of the device on the specified channel
-    # The result is returned in a measure of quarter-microseconds, which mirrors
-    # the Target parameter of set_target.
-    # This is not reading the true servo position, but the last target position sent
-    # to the servo. If the Speed is set to below the top speed of the servo, then
-    # the position result will align well with the acutal servo position, assuming
-    # it is not stalled or slowed.
-    def getPosition(self, chan):
+
+    def get_position(self, chan):
+        """
+        Get the current position of the device on the specified channel
+        The result is returned in a measure of quarter-microseconds, which mirrors
+        the Target parameter of set_target.
+        This is not reading the true servo position, but the last target position sent
+        to the servo. If the Speed is set to below the top speed of the servo, then
+        the position result will align well with the acutal servo position, assuming
+        it is not stalled or slowed.
+        """
         cmd = chr(0x10) + chr(chan)
         self.send(cmd)
-        timeout = 1
-
+        self.timeout = 1
         start_time = time.time()
-        # your code
-
-        while time.time() - start_time < timeout:
+        while time.time() - start_time < self.timeout:
             if self.usb.in_waiting == 2:
                 lsb = ord(self.usb.read())
                 msb = ord(self.usb.read())
                 return ((msb << 8) + lsb) / 4
-        if time.time() - start_time > timeout:
+        if time.time() - start_time > self.timeout:
             print('read timed out')
         return -1
 
     def get_all_positions(self):
-        return [self.getPosition(x) in range(0, 5)]
+        return [self.get_position(x) for x in range(0, 24)]
 
     # Test to see if a servo has reached the set target position.  This only provides
     # useful results if the Speed parameter is set slower than the maximum speed of
-    # the servo.  Servo range must be defined first using setRange. See setRange comment.
+    # the servo.  Servo range must be defined first using set_range. See set_range comment.
     #
     # ***Note if target position goes outside of Maestro's allowable range for the
     # channel, then the target can never be reached, so it will appear to always be
     # moving to the target.
     def isMoving(self, chan):
         if self.target_positions[chan] > 0:
-            if abs(self.getPosition(chan) - self.target_positions[chan]) > 10:
+            if abs(self.get_position(chan) - self.target_positions[chan]) > 10:
                 return 1
         return 0
 
@@ -271,3 +324,12 @@ def calculate_movement_time(pwm_ms, speed_deg_per_sec=0):
     deg_per_ms = 180 / (angle_180deg - angle_0deg)
     travel_angle = (pwm_ms * deg_per_ms) 
     return travel_angle * angular_speed_sec_per_degree
+
+def get_slowest_movement_time(new_vector, old_vector, speed):
+    speed_us_per_ms = [ 0.25 * v / 10.0 for v in speed] 
+    delta_pwm_us = [abs(a-b) for a,b in zip(new_vector, old_vector)]
+    slowest_movement_at_speed = max([s*t for s,t in zip(speed_us_per_ms, delta_pwm_us)]) / 1000.0
+    slowest_movement_at_speed_0 = calculate_movement_time(get_max_angle(new_vector, old_vector))
+    return max([slowest_movement_at_speed, slowest_movement_at_speed_0])
+
+    
